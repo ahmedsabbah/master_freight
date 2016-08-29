3cd237faddf7afd86f20c6f65e0f1415453e6c65
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-08-29 21:59:53 +0200

    adding deployment setup

--- master_freight/settings.py
+++ master_freight/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import dj_database_url
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -25,7 +28,6 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -50,8 +52,24 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'master_freight.urls'
 
-WSGI_APPLICATION = 'master_freight.wsgi.application'
+TEMPLATES = (
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG,
+        },
+    },
+)
 
+WSGI_APPLICATION = 'master_freight.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -61,23 +79,73 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'ispark_db',
+    #     'USER': 'enlighthost',
+    #     'PASSWORD': 'Pass123',
+    #     'HOST': '',
+    #     'PORT': '',
+    # }
 }
+
+# AUTH_PASSWORD_VALIDATORS = (
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# )
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Africa/Cairo'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.privateemail.com'
+EMAIL_PORT=465
+EMAIL_HOST_USER='info@isparkegypt.com'
+EMAIL_HOST_PASSWORD='ispark2015'
+EMAIL_USE_SSL=True
+# EMAIL_USE_TLS=True
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+# STATIC_ROOT = '/home/enlighthost/webapps/static/'
+# STATIC_URL = 'http://www.isparkegypt.com/static/'
+
+# MEDIA_ROOT = '/home/enlighthost/webapps/media/'
+# MEDIA_URL = 'http://www.isparkegypt.com/media/'
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
--- master_freight/wsgi.py
+++ master_freight/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+
+# site.addsitedir('/home/master_freight/webapps/master_freight/lib/python2.7/site-packages')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "master_freight.settings")
+
+# activate_this = os.path.expanduser("~/webapps/master_freight/bin/activate_this.py")
+# execfile(activate_this, dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
