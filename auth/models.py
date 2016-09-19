504a38d232500f3438d629b31f6e61a63f0f2b6a
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-09-19 22:14:00 +0200

    templates renamed and model user added

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from django.db.models.signals import post_save,post_delete, pre_save
+from django.dispatch import receiver
+
+from managers import UserManager
+
+class AbstractUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), blank=False, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=False)
+    last_name = models.CharField(_('last name'), max_length=30, blank=False)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+        abstract = True
+
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        'Returns the short name for the user.'
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+class User(AbstractUser):
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+    def __str__(self):
+        return self.get_full_name()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    return render(request, 'quotations_lcl.html', {})
+
+def home(request):
+    return render(request, 'quotations_lcl.html')
+
+def jobs(request):
+    if request.user.type == 'sales':
+        return render(request, 'Quotations-LCL.html')
+    elif request.user.type == 'operations':
+        return render(request, 'Quotations-LCL.html')
+    elif request.user.type == 'accounting':
+        return render(request, 'Quotations-LCL.html')
+    elif request.user.type == 'hr':
+        return render(request, 'Quotations-LCL.html')
+    elif request.user.type == 'admin':
+        return render(request, 'Quotations-LCL.html')
+    else:
+        return render(request, '404.html')
